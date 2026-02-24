7bebc1ece7fd910813610d7712f8ba6a279981bc
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-02-24 19:08:36 +0530

    create match error handling

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,7 +1,27 @@
 import { Router } from "express";
+import { createMatchSchema } from "../validation/matches";
 
 export const matchRouter = Router();
 
 matchRouter.get("/", (req, res, next) => {
   res.status(200).json({ message: "Matches List" });
+});
+
+matchRouter.post("/", (req, res, next) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      error: "Invalid payload.",
+      details: JSON.stringify(parsed.error),
+    });
+  }
+
+  try {
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to create match.",
+      details: JSON.stringify(error),
+    });
+  }
 });
