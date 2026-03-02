100d554f4be78497837a9ce04d500e5ca72e4bab
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-03-02 14:14:16 +0530

    add match creation route with validation and status logic

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { createMatchSchema } from "../validation/matches";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+import { getMatchStatus } from "../../utils/match-status";
 
 export const matchRouter = Router();
 
@@ -7,7 +10,7 @@ matchRouter.get("/", (req, res, next) => {
   res.status(200).json({ message: "Matches List" });
 });
 
-matchRouter.post("/", (req, res, next) => {
+matchRouter.post("/", async (req, res, next) => {
   const parsed = createMatchSchema.safeParse(req.body);
 
   if (!parsed.success) {
@@ -17,7 +20,24 @@ matchRouter.post("/", (req, res, next) => {
     });
   }
 
+  const {
+    data: { startTime, endTime, homeScore, awayScore },
+  } = parsed;
+
   try {
+    const [event] = await db
+      .insert(matches)
+      .values({
+        ...parsed.data,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        homeScore: homeScore ?? 0,
+        awayScore: awayScore ?? 0,
+        status: getMatchStatus(startTime, endTime),
+      })
+      .returning();
+
+    res.status(201).json({ data: event });
   } catch (error) {
     res.status(500).json({
       error: "Failed to create match.",
